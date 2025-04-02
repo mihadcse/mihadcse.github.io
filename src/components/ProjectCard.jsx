9e0cfdcdfa5aa9c484212fa5f5a0353fbb4c1525
mihadcse <huzzatullah@iut-dhaka.edu>
2025-04-02 16:04:21 +0600

    icon change

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-
-// const ProjectCard = ({ title, description, link, image }) => {
-//   return (
-//     <div className="w-80 md:w-96 h-96 flex flex-col border-2 hover:text-black rounded-lg  border-cyan-400 hover:shadow-2xl hover:shadow-cyan-500 hover:bg-cyan-600 transition duration-300 bg-cyan-950 shadow-lg shadow-cyan-500">
-//       <img src={image} alt={title} className="w-full h-2/3 object-cover rounded-t-lg pb-3 p-1" />
-//       <h3 className="text-2xl font-semibold text-cyan-200">{title}</h3>
-//       <p className="mt-2 text-white">{description}</p>
-//       <a 
-//         href={link} 
-//         target="_blank" 
-//         rel="noopener noreferrer" 
-//         className="text-blue-600 mt-4 inline-block hover:underline"
-//       >
-//         View Project
-//       </a>
-//       <br />
-      
-//     </div>
-    
-//   );
-// };
-
-// export default ProjectCard;
-
 import React from "react";
 import { FaGithub, FaYoutube } from "react-icons/fa";
 
